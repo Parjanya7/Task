51ba903890a4a76f3d7e6fb1aca9d85171e8ab99
Parjanya7 <57247224+Parjanya7@users.noreply.github.com>
2019-12-08 20:11:13 +0530

    App.js
    Main rendering js file of react.

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Upload from './Upload';
+import './Style.css';
+
+function App() {
+  return (
+    <div id = "upload">
+      <h1> Hi..</h1>
+      <Upload/>
+    </div>
+  );
+}
+
+export default App;
